356dab592f2b8b533b5331a4d255f8e892e065d3
ajosephy <alex.s.josephy@gmail.com>
2022-02-28 20:36:59 -0500

    Merge remote-tracking branch 'origin/main' into 58-pipeline-decorator

--- chime_frb_api/workflow/__init__.py
+++ chime_frb_api/workflow/__init__.py
@@ -1,2 +1,3 @@
 """Top-level imports for Tasks API."""
 from .work import Work  # noqa: F401
+from .pipelining import pipeline
--- chime_frb_api/workflow/pipelining.py
+++ chime_frb_api/workflow/pipelining.py
@@ -1,1 +1,40 @@
 """Pipeline any method to be compatible with Tasks API."""
+
+from functools import wraps
+
+from .work import Work
+
+def pipeline(name=None, lifetime=-1, retry=True):
+    def _decorator(user_func):
+        @wraps(user_func)  # propagates func name & doc-string
+        def _wrapper(*args, **kwargs):
+            try:
+                work = Work.fetch(name)
+                output = func(**work.parameters)
+                results, products, plots = func(*args, **kwargs)
+                work.results = results
+                work.products = products
+                work.plots = plots
+                work.status = True
+            except:
+                work.status(False)
+            finally:
+                work.complete(str(work.Status))
+
+
+        return _wrapper
+    return _decorator
+
+'''
+global PRECURSORS
+try:
+    work = tasks.fetch(pipeline_name)
+    results, products, plots = func(**work.parameters)
+    work.status = True
+    work.precursors = PRECURSORS
+except:
+    work.status = False
+finally:
+    task.complete(work)
+    PRECURSORS = {}
+'''

788100e2ed76095a0e565ee7607d4a5231bd36be
Paul Scholz <paul.scholz@dunlap.utoronto.ca>
2022-04-05 11:55:12 -0400

    refactor(workflow): Improved implemenation of transfer daemon

--- chime_frb_api/workflow/daemons/transfer.py
+++ chime_frb_api/workflow/daemons/transfer.py
@@ -7,33 +7,22 @@ def transfer_work(limit_per_run=1000):
     buckets = Buckets()
     results = Results()
 
-    successful_work = buckets.view(
-        query = {"status": "success"},
-        projection = {},
-        skip = 0, 
-        limit = limit_per_run,
-    )
-
-    # do we need to deposit per pipeline? If so need to sort the successful work
-    # by pipeline
-
-    # rewrite this to make a list of successful work per pipeline instead of 
-    # doing one deposit per item of work.
-    pipelines = []
-    for work in successful_work:
-        pipelines.append(work.pipeline)
-
-    pipelines = list(set(pipelines))
+    # do we need to deposit results per pipeline? Assuming so for now.
+    pipelines = buckets.pipelines()
 
     for pipeline in pipelines:
-        for work in successful_work:
-            if work.pipeline == pipeline:
-                status = results.deposit([work], pipeline=pipeline)
-                if status ==True: # is this enough to confirm its deposited?
-                    buckets.delete_ids([work.id])
 
-    #somehow confirm that results were deposited?
+        successful_work = buckets.view(
+            query = {"status": "success", "pipeline": pipeline},
+            projection = {}, # do we want everything?
+            skip = 0, 
+            limit = limit_per_run,
+        )
 
+        if len(successful_work):
+            status = results.deposit(successful_work, pipeline=pipeline)
+            if status ==True: # is this enough to confirm its deposited?
+                buckets.delete_ids([work.id for work in successful_work])
 
 if __name__ == "__main__":
     transfer_work()

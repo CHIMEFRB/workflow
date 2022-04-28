d7ee97e07052ecdef167a19ee9db7532f0eb6a47
Paul Scholz <paul.scholz@dunlap.utoronto.ca>
2022-04-28 15:03:43 -0400

    fix(transfer daemon): Updated transfer daemon to work with results

--- chime_frb_api/workflow/daemons/transfer.py
+++ chime_frb_api/workflow/daemons/transfer.py
@@ -7,22 +7,17 @@ def transfer_work(limit_per_run=1000):
     buckets = Buckets()
     results = Results()
 
-    # do we need to deposit results per pipeline? Assuming so for now.
-    pipelines = buckets.pipelines()
+    successful_work = buckets.view(
+        query = {"status": "success"},
+        projection = {}, # do we want everything?
+        skip = 0, 
+        limit = limit_per_run,
+    )
 
-    for pipeline in pipelines:
-
-        successful_work = buckets.view(
-            query = {"status": "success", "pipeline": pipeline},
-            projection = {}, # do we want everything?
-            skip = 0, 
-            limit = limit_per_run,
-        )
-
-        if len(successful_work):
-            status = results.deposit(successful_work, pipeline=pipeline)
-            if status ==True: # is this enough to confirm its deposited?
-                buckets.delete_ids([work.id for work in successful_work])
+    if len(successful_work):
+        status = results.deposit(successful_work)
+        if status == True: # is this enough to confirm its deposited?
+            buckets.delete_ids([work.id for work in successful_work])
 
 if __name__ == "__main__":
     transfer_work()

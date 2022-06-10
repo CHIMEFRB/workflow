f86b9585c2c18199fda94789356ffbbb2158abf0
Siqi Liu <siqiliu043@gmail.com>
2022-06-10 12:07:06 -0400

    refactor(audit daemon): remove redundant args

--- chime_frb_api/workflow/daemons/audit.py
+++ chime_frb_api/workflow/daemons/audit.py
@@ -1,20 +1,15 @@
-from time import time
-
 from chime_frb_api.modules.buckets import Buckets
 
 
-def audit_work(limit_per_run=1000,**kwargs):
+def audit_work(**kwargs):
     """Audit the Buckets DB for work that is failed, expired, or stale work.
 
     Args:
-        limit_per_run (int): Max number of failed Work entires to delete per
-        run of daemon.
         **kwargs (Dict[str, Any]): Keyword arguments for the Buckets API.
     """
 
     buckets = Buckets(**kwargs)
-    audit_results = buckets.audit()
-    return audit_results
+    return buckets.audit()
 
 if __name__ == "__main__":
     audit_work()

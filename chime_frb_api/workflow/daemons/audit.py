bba327d63a98496e3f077d9e1e02faf5d6e613ae
Siqi Liu <siqiliu043@gmail.com>
2022-06-10 12:01:00 -0400

    feat(audit daemon): remove deletion from audit daemon scope

--- chime_frb_api/workflow/daemons/audit.py
+++ chime_frb_api/workflow/daemons/audit.py
@@ -1,5 +1,7 @@
-from chime_frb_api.modules.buckets import Buckets
 from time import time
+
+from chime_frb_api.modules.buckets import Buckets
+
 
 def audit_work(limit_per_run=1000,**kwargs):
     """Audit the Buckets DB for work that is failed, expired, or stale work.
@@ -11,33 +13,7 @@ def audit_work(limit_per_run=1000,**kwargs):
     """
 
     buckets = Buckets(**kwargs)
-
-    # the buckets.audit call:
-    # 1) failed: retries 'failure' work if attempts < retries 
-    # 2) expired: fails 'running' work with run time > timeout
-    # 3) stale: fails all work that is older than 31 days
     audit_results = buckets.audit()
-
-    # then want to delete failed work that has used up retries
-    failed_work = buckets.view(
-        query = {
-            "status": "failure",
-            "$expr": {"$gte": ["attempt","retries"]},
-            "$expr": {"$lt": ["stop", time() - 86400]},
-        },
-        projection = {"id": True},
-        skip = 0,
-        limit = limit_per_run,
-    )
-    print(failed_work)
-        
-    audit_results["deleted"] = 0
-    if len(failed_work) > 0:
-        response = buckets.delete_ids(failed_work)
-        print(response)
-        if response == True:
-            audit_results["deleted"] = len(failed_work)
-
     return audit_results
 
 if __name__ == "__main__":

b5fc5136d020b479cebbf67dae9948c48f06137f
Siqi Liu <siqiliu043@gmail.com>
2022-06-10 16:01:45 -0400

    feat(transfer daemon): return dict results instead of bool

--- chime_frb_api/workflow/daemons/transfer.py
+++ chime_frb_api/workflow/daemons/transfer.py
@@ -11,14 +11,18 @@ def deposit_work_to_results(buckets, results, works):
         buckets (Buckets): Buckets module.
         results (Results): Results module.
         works (List[Dict[str, Any]]): Work to deposit.
+
+    Returns:
+        transferred_count (int): Number of works deposited to results.
     """
+    transferred_count = 0
     if len(works) == 0:
-        return True
+        return transferred_count
     results_deposit_status = results.deposit(works)
-    if all(results_deposit_status.values()) == True:
+    transferred_count = sum(results_deposit_status.values())
+    if transferred_count == len(works):
         buckets.delete_ids([work["id"] for work in works])
-        return True
-    return False
+    return transferred_count
 
 def transfer_work(limit_per_run=1000,buckets_kwargs={},results_kwargs={}):
     """Transfer successful Work from Buckets DB to Results DB
@@ -28,9 +32,13 @@ def transfer_work(limit_per_run=1000,buckets_kwargs={},results_kwargs={}):
         run of daemon.
         buckets_kwargs (Dict[str, Any]): Keyword arguments for the Buckets API.
         results_kwargs (Dict[str, Any]): Keyword arguments for the Results API.
+
+    Returns:
+        transfer_status (Dict[str, Any]): Transfer results.
     """
     buckets = Buckets(**buckets_kwargs)
     results = Results(**results_kwargs)
+    transfer_status = {}
     # 1. Transfer successful Work
     # TODO: decide projection fields
     successful_work = buckets.view(
@@ -39,20 +47,22 @@ def transfer_work(limit_per_run=1000,buckets_kwargs={},results_kwargs={}):
         skip=0,
         limit=limit_per_run,
     )
-    deposit_successful_work_status = deposit_work_to_results(buckets, results, successful_work)
+    transfer_successful_work_count = deposit_work_to_results(buckets, results, successful_work)
+    transfer_status["successful_work_transferred"] = transfer_successful_work_count
 
     # 2. Transfer failed Work
     # TODO: decide projection fields
     failed_work = buckets.view(
         query={
             "status": "failure",
-            "$expr": {"$gte": ["attempt","retries"]},
+            "$expr": {"$gte": ["$attempt","$retries"]},
         },
         projection={},
         skip=0,
         limit=limit_per_run,
     )
-    deposit_failed_work_status = deposit_work_to_results(buckets, results, failed_work)
+    transfer_failed_work_count = deposit_work_to_results(buckets, results, failed_work)
+    transfer_status["failed_work_transferred"] = transfer_failed_work_count
 
     # 3. Delete stale Work (cut off time: 14 days)
     cutoff_creation_time = time.time() - (60 * 60 * 24 * 14)
@@ -67,7 +77,8 @@ def transfer_work(limit_per_run=1000,buckets_kwargs={},results_kwargs={}):
     )
     if len(stale_work) > 0:
         buckets.delete_ids([work["id"] for work in stale_work])
-    return deposit_successful_work_status and deposit_failed_work_status
+    transfer_status["stale_work_deleted"] = len(stale_work)
+    return transfer_status
 
 
 if __name__ == "__main__":

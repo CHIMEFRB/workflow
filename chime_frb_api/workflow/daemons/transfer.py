9e9b7aa136c4a762d38f6513793dbb1e3e020ddd
Shiny Brar <charanjotbrar@gmail.com>
2022-06-12 13:55:00 -0400

    refactor(audit): cleaned up code

--- chime_frb_api/workflow/daemons/transfer.py
+++ chime_frb_api/workflow/daemons/transfer.py
@@ -1,11 +1,15 @@
+"""Transfer Daemon."""
 import time
+from typing import Any, Dict, List
 
 from chime_frb_api.modules.buckets import Buckets
 from chime_frb_api.modules.results import Results
 
 
-def deposit_work_to_results(buckets, results, works):
-    """Deposit work to results, and remove them from buckets
+def deposit_work_to_results(
+    buckets: Buckets, results: Results, works: List[Dict[str, Any]]
+) -> int:
+    """Deposit work to results, and remove them from buckets.
 
     Args:
         buckets (Buckets): Buckets module.
@@ -15,7 +19,7 @@ def deposit_work_to_results(buckets, results, works):
     Returns:
         transferred_count (int): Number of works deposited to results.
     """
-    transferred_count = 0
+    transferred_count: int = 0
     if len(works) == 0:
         return transferred_count
     results_deposit_status = results.deposit(works)
@@ -24,8 +28,13 @@ def deposit_work_to_results(buckets, results, works):
         buckets.delete_ids([work["id"] for work in works])
     return transferred_count
 
-def transfer_work(limit_per_run=1000,buckets_kwargs={},results_kwargs={}):
-    """Transfer successful Work from Buckets DB to Results DB
+
+def transfer_work(
+    limit_per_run: int = 1000,
+    buckets_kwargs: Dict[str, Any] = {},
+    results_kwargs: Dict[str, Any] = {},
+) -> Dict[str, Any]:
+    """Transfer successful Work from Buckets DB to Results DB.
 
     Args:
         limit_per_run (int): Max number of failed Work entires to transfer per
@@ -47,7 +56,9 @@ def transfer_work(limit_per_run=1000,buckets_kwargs={},results_kwargs={}):
         skip=0,
         limit=limit_per_run,
     )
-    transfer_successful_work_count = deposit_work_to_results(buckets, results, successful_work)
+    transfer_successful_work_count = deposit_work_to_results(
+        buckets, results, successful_work
+    )
     transfer_status["successful_work_transferred"] = transfer_successful_work_count
 
     # 2. Transfer failed Work
@@ -55,7 +66,7 @@ def transfer_work(limit_per_run=1000,buckets_kwargs={},results_kwargs={}):
     failed_work = buckets.view(
         query={
             "status": "failure",
-            "$expr": {"$gte": ["$attempt","$retries"]},
+            "$expr": {"$gte": ["$attempt", "$retries"]},
         },
         projection={},
         skip=0,

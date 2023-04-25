193ee016e2caf25f983146ff33bd641aac739130
Shiny Brar  (he/il) <charanjotbrar@gmail.com>
2023-04-25 11:42:06 -0400

    feat(workflow): added utility to check if the script is running from k8s or docker runtime

---
+++ chime_frb_api/workflow/lifecycle/container.py
@@ -0,0 +1,19 @@
+"""Check if the process is running in a container."""
+
+
+def virtualization() -> str:
+    """Check if the process is running in a container.
+
+    Returns:
+        str: Container type
+    """
+    try:
+        with open("/proc/1/cgroup") as f:
+            for line in f.readlines():
+                if "docker" in line:
+                    return "docker"
+                elif "kubepod" in line:
+                    return "kubernetes"
+    except Exception:
+        pass
+    return "none"

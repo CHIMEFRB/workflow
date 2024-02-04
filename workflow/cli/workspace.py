d8f6802066a4a194cfea17bbad8964b0f392f065
Shiny Brar  (he/il) <charanjotbrar@gmail.com>
2024-02-04 06:23:20 -0500

    fix(workspace): improved the workspace set/unset logic

--- workflow/cli/workspace.py
+++ workflow/cli/workspace.py
@@ -9,14 +9,19 @@ from rich.console import Console
 from rich.table import Table
 from yaml import dump
 
-from workflow import CONFIG_PATH, MODULE_PATH
+from workflow import DEFAULT_WORKSPACE_PATH, MODULE_PATH
 from workflow.utils import read as reader
+from workflow.utils.logger import get_logger
+
+logger = get_logger("workflow.cli.workspace")
 
 pretty.install()
 console = Console()
 
-# Local Workflow configurations are kept in ~/.workflow/
-localspaces: Path = CONFIG_PATH / "workspaces"
+localspaces = Path(DEFAULT_WORKSPACE_PATH).parent
+localstems = [space.stem for space in localspaces.glob("*.y*ml")]
+modulespaces = Path(MODULE_PATH, "workflow", "workspaces")
+modulestems = [space.stem for space in modulespaces.glob("*.y*ml")]
 
 
 @click.group(name="workspace", help="Manage workflow workspaces.")
@@ -30,25 +35,28 @@ def ls():
     """List all workspaces."""
     table = Table(
         title="\nWorkflow Workspaces",
-        show_header=True,
         header_style="magenta",
         title_style="bold magenta",
     )
     table.add_column("Workspace", style="cyan", justify="right")
     table.add_column("Source", style="green", justify="left")
-    table.add_row("", "from workflow package", style="red italic")
-    for workspace in Path(MODULE_PATH, "workflow", "workspaces").glob("*.y*ml"):
+    table.add_row("", "From Workflow Python Module", style="red italic")
+    for workspace in modulespaces.glob("*.y*ml"):
         table.add_row(workspace.stem, workspace.as_posix())
-    table.add_row("", "from config folder", style="red italic")
+    table.add_row("", "From Local Configuration Folder", style="red italic")
     for workspace in localspaces.glob("*.y*ml"):
         table.add_row(workspace.stem, workspace.as_posix())
     console.print(table)
 
 
 @workspace.command("set", help="Set the active workspace.")
-@click.option("--debug", is_flag=True, help="Print debug information.")
-@click.argument("workspace", type=str, required=True, nargs=1)
-def set(workspace: str, debug: bool = False):
+@click.argument(
+    "workspace",
+    type=str,
+    required=True,
+    nargs=1,
+)
+def set(workspace: str):
     """Set the active workspace.
 
     Args:
@@ -57,42 +65,63 @@ def set(workspace: str, debug: bool = False):
             a local workspace.
         debug (bool, optional): Debug Logs. Defaults to False.
     """
-    console.print(f"Retrieving {workspace}", style="italic blue")
-    config: Dict[str, Any] = reader.workspace(workspace)
+    config: Dict[str, Any] = {}
+    console.print(f"Locating workspace {workspace}", style="italic blue")
+    if Path(workspace).absolute().exists():
+        console.print(f"Reading {workspace}", style="italic blue")
+        config = reader.workspace(workspace)
+    elif workspace in localstems:
+        for possibility in localspaces.glob(f"{workspace}.y*ml"):
+            console.print(f"Reading {possibility}", style="italic blue")
+            config = reader.workspace(possibility)
+            break
+    elif workspace in [space.stem for space in modulespaces.glob("*.y*ml")]:
+        for possibility in modulespaces.glob(f"{workspace}.y*ml"):
+            console.print(f"Reading {possibility}", style="italic blue")
+            config = reader.workspace(possibility)
+            break
+    else:
+        console.print(f"Workspace {workspace} not found.", style="bold red")
+        return
+
     name: str = config["workspace"]
-    console.print(f"Discovered workspace {name}", style="italic blue")
-    if debug:
-        console.print(config, style="green")
-    # Create the ~/.workflow/workspaces/
     localspaces.mkdir(parents=True, exist_ok=True)
-    # Copy the workspace to ~/.workflow/workspaces/<name>.yml
-    console.print(f"Writing {localspaces / f'{name}.yml'}", style="italic green")
-    with open(localspaces / f"{name}.yml", "w") as filename:
+    # Copy the workspace config to ~/.workflow/workspaces/<name>.yml
+    configpath = localspaces / f"{name}.yml"
+    activepath = localspaces / "active.yml"
+    # Write config to configpath, even if it already exists.
+    with open(configpath, "w") as filename:
         dump(config, filename)
-    console.print(f"Writing {localspaces / 'active.yml'}", style="italic green")
-    # Copy the workspace to ~/.workflow/workspaces/active.yml
-    with open(localspaces / "active.yml", "w") as filename:
+        console.print(f"Copied {name} to {configpath}.", style="bold green")
+    # Write config to activepath, even if it already exists.
+    with open(activepath, "w") as filename:
         dump(config, filename)
-    console.print(f"Workspace {name} set.", style="bold green")
+        console.print(f"Workspace {name} set to active.", style="bold green")
 
 
 @workspace.command("read", help="Read workspace config.")
-@click.option(
-    "--source", "-s", type=str, required=True, help="Source can be url, path, or name"
-)
-def read(source: str):
-    """Read the workspace config.
+@click.argument("workspace", type=str, required=True, nargs=1, default="active")
+def read(workspace: str):
+    """Read the active workspace.
 
     Args:
-        source (str): The source of the workspace config.
+        workspace (str): The workspace to read.
     """
-    try:
-        if source == "active":
-            source = (localspaces / "active.yml").as_posix()
-        config: Dict[str, Any] = reader.workspace(source)
-        console.print(config, style="green")
-    except Exception as error:
-        console.print(error, style="red")
+    if workspace in localstems:
+        for possibility in localspaces.glob(f"{workspace}.y*ml"):
+            console.print(f"Reading {possibility}", style="italic blue")
+            config = reader.workspace(possibility)
+            console.print(config, style="green")
+            return
+    elif workspace in modulestems:
+        for possibility in modulespaces.glob(f"{workspace}.y*ml"):
+            console.print(f"Reading {possibility}", style="italic blue")
+            config = reader.workspace(possibility)
+            console.print(config, style="green")
+            return
+    else:
+        console.print(f"Workspace {workspace} not found.", style="bold red")
+        return
 
 
 @workspace.command("unset", help="Unset active workspace.")
--- workflow/utils/read.py
+++ workflow/utils/read.py
@@ -1,55 +1,58 @@
 """Read various workflow configurations."""
 
+import re
 from pathlib import Path
-from typing import Any
+from typing import Any, Dict
 
 from requests import get
 from requests.exceptions import RequestException
 from yaml import safe_load
 
-from workflow import MODULE_PATH
 from workflow.utils.logger import get_logger
 
 logger = get_logger("workflow.utils.read")
 
 
-def workspace(source: str) -> Any:
+def workspace(source: str | Path) -> Dict[str, Any]:
     """Read a namespace YAML file.
 
     Args:
-        source (str): Source of the namespace.
+        source (str | Path): Source of the workspace.
             Can be a URL, a file path, or a namespace name.
 
     Returns:
         Any: The namespace contents.
     """
-    # Check if the source provided is a URL
-    if source.startswith("http") or source.startswith("https"):
-        logger.debug(f"source url: {source}")
-        return url(source)
+    if isinstance(source, Path):
+        if source.exists():
+            if source.suffix in [".yaml", ".yml"]:
+                logger.debug(f"workspace @ {source.as_posix()}")
+                return filename(source.as_posix())
+            else:
+                msg = f"{source} is not a valid yaml file."
+                logger.error(msg)
+                raise ValueError(msg)
 
-    # Check if the source provided is a valid file path
-    if Path(source).exists():
-        logger.debug(f"source path: {source}")
-        return filename(source)
-
-    for workspace in Path(MODULE_PATH, "workflow", "workspaces").glob("*.y*ml"):
-        if workspace.stem == source:
-            logger.debug(f"source path: {workspace.as_posix()}")
-            return filename(workspace.as_posix())
+    if isinstance(source, str):
+        if is_valid_url(source):
+            logger.debug(f"workspace @ {source}")
+            return url(source)
 
     logger.error(f"workspace: {source} does not exist.")
     raise ValueError(f"workspace {source} does not exist.")
 
 
 def url(source: str) -> Any:
-    """Read a URL.
+    """Read a source URL.
 
     Args:
         source (str): The URL to read.
 
+    Raises:
+        error: If the request fails.
+
     Returns:
-        Any: The URL JSON response.
+        Any: The source contents.
     """
     try:
         response = get(source)
@@ -66,6 +69,9 @@ def filename(source: str) -> Any:
     Args:
         source (str): The filename to read.
 
+    Raises:
+        error: If the file cannot be read.
+
     Returns:
         Any: The source contents.
     """
@@ -75,3 +81,30 @@ def filename(source: str) -> Any:
     except Exception as error:
         logger.exception(error)
         raise error
+
+
+def is_valid_url(url: str) -> bool:
+    """Return True if the URL is valid.
+
+    Args:
+        url (str): The URL to validate.
+
+    Returns:
+        bool: True if the URL is valid.
+    """
+    regex = re.compile(
+        r"^(https?://)?"  # http:// or https:// (optional)
+        r"("
+        r"localhost|"  # localhost
+        r"((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}"  # ipv4
+        r"(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"  # ipv4
+        r"|"
+        r"([a-zA-Z0-9]+([-.\w]*[a-zA-Z0-9])*"  # domain...
+        r"(\.[a-zA-Z0-9]{2,3})+)"  # ...with top level domain
+        r")"
+        r"(?::\d{2,5})?"  # optional port
+        r"(?:/?|[/?]\S+)?$",
+        re.IGNORECASE,
+    )  # optional trailing path/query
+
+    return re.match(regex, url) is not None

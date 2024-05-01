303bb26018d60783489275cf34cafaad792433ba
Oswaldo Alcalá <odarotto@gmail.com>
2024-05-01 10:57:39 -0400

    feat(variables.py): adding missing statuses

--- workflow/utils/variables.py
+++ workflow/utils/variables.py
@@ -1,11 +1,12 @@
 """Variables needed for console printing."""
 
 status_colors = {
-    "active": "bright_blue",
-    "running": "blue",
     "created": "lightblue",
     "queued": "yellow",
+    "active": "bright_blue",
+    "running": "blue",
     "success": "green",
+    "paused": "orange",
     "failure": "red",
     "cancelled": "dark_goldenrod",
     "expired": "dark_goldenrod",
@@ -13,9 +14,11 @@ status_colors = {
 
 status_symbols = {
     "created": "\U000026AA",  # white
+    "queued": "\u23F3",  # hourglass
     "active": "\U0001F7E2",  # green
-    "paused": "\U0001F7E1",  # yellow
+    "running": "\u2699",  # gear
     "success": "\U00002705",  # Green check
+    "paused": "\U0001F7E1",  # yellow
     "failure": "\U0000274C",  # cross mark
     "cancelled": "\U0001F534",  # red
 }
